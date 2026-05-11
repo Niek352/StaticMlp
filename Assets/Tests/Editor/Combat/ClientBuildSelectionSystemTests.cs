1aabbc467ac9e4afc5197fd3400302d3586e4d4f
Pavel <oavelasdqwe8@gmail.com>
2026-05-11 10:07:20 +0300

    Fix

--- Assets/Tests/Editor/Combat/ClientBuildSelectionSystemTests.cs
+++ Assets/Tests/Editor/Combat/ClientBuildSelectionSystemTests.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using StaticMlp.Features.Build;
 using StaticMlp.Features.Combat;
 using StaticMlp.Game.Input;
+using StaticMlp.Networking;
 using UnityEngine;
 
 namespace StaticMlp.Tests.Combat
